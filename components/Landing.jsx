8b6f14be2280148ca677f0cb7f15e4d277fccc4d
Aaron Kurian <aaronkurianabraham@gmail.com>
2025-03-05 16:45:36 +0530

    button ready

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -454,28 +454,26 @@ const Landing = () => {
               priority
             />
           </motion.div>
-
-
-          <div className="absolute inset-x-0 w-full top-[35rem] sm:top-[30rem] mx-auto flex justify-center">
-            <motion.div
-              className="flex justify-center items-center w-full z-10 mb-[210px] sm:mb-[40px] sm:-mx-12 md:mb-[-10px] md:-mx-8 lg:mx-2"
-              initial={{ opacity: 0, y: 100 }}
-              animate={{
-                opacity: 1,
-                y: 0,
-              }}
-              transition={{
-                duration: 1,
-                ease: "easeInOut",
-              }}
-            >
-              <div 
-                className="apply-button" 
-                data-hackathon-slug="tinkhack-mec"
-                data-button-theme="dark-inverted" 
-                style={{ height: '40px', width: window.innerWidth < 640 ? '180px' : '250px' }}
-              ></div>
-            </motion.div>
+          <div className="absolute flex justify-center items-center w-full bottom-[10%] z-10 mb-[210px] sm:mb-[40px] sm:-mx-12 md:mb-[-10px] md:-mx-8 lg:mx-2">
+          <motion.div
+            className="w-[10%] overflow-hidden flex justify-center items-center"
+            initial={{ opacity: 0, y: 100 }}
+            animate={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{
+              duration: 1,
+              ease: "easeInOut",
+            }}
+          >
+            <div 
+              className="apply-button" 
+              data-hackathon-slug="tinkhack-mec"
+              data-button-theme="dark-inverted" 
+              style={{ height: '44px', width: '312px' }}
+            ></div>
+          </motion.div>
           </div>
         
       </div>
